da63d19736bd00e3956996d5091a679fed7fb52b
abhishekkumar71 <151723904+abhishekkumar71@users.noreply.github.com>
2025-08-11 12:41:15 +0530

    added popular sessions

--- frontend/src/components/PopularSection.jsx
+++ frontend/src/components/PopularSection.jsx
@@ -12,26 +12,46 @@ const sessions = [
     title: "Morning Meditation",
     desc: "Start your day with calm.",
     author: "Anjali R.",
+    action: () => {
+      navigate(
+        "https://healthhub-ud5p.onrender.com/session/6897801705cd724f5bf89269"
+      );
+    },
   },
   {
     title: "Evening Reflections",
     desc: "Relax and rewind your thoughts.",
     author: "Ravi M.",
+    action: () => {
+      navigate(
+        "https://healthhub-ud5p.onrender.com/session/68982a9d18ccd6fa406eaef4"
+      );
+    },
   },
   {
     title: "Focus Booster",
     desc: "Short session to sharpen your attention.",
     author: "Arjun D.",
+    action: () => {
+      navigate(
+        "https://healthhub-ud5p.onrender.com/session/68989a2d4e2f825427ae64dd"
+      );
+    },
   },
   {
     title: "Stress Release",
     desc: "Let go of tension and restore peace.",
     author: "Aditya S.",
+    action: () => {
+      navigate(
+        "https://healthhub-ud5p.onrender.com/session/68989c764e2f825427ae6525"
+      );
+    },
   },
 ];
 
 export default function PopularSessions() {
-    const navigate=useNavigate();
+  const navigate = useNavigate();
   return (
     <Container sx={{ py: 6 }}>
       <Typography variant="h5" fontWeight="bold" gutterBottom>
@@ -46,6 +66,7 @@ export default function PopularSessions() {
                   boxShadow: 7,
                 },
               }}
+              onClick={s.action}
             >
               <CardContent>
                 <Typography variant="h6">{s.title}</Typography>
@@ -63,8 +84,8 @@ export default function PopularSessions() {
       <Button
         variant="contained"
         size="large"
-        sx={{ backgroundColor: "#075b07",padding:"10px",marginTop:"15px" }}
-        onClick={()=>navigate("/explore")}
+        sx={{ backgroundColor: "#075b07", padding: "10px", marginTop: "15px" }}
+        onClick={() => navigate("/explore")}
       >
         View more
       </Button>{" "}
